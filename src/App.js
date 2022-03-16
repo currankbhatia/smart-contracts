a16322e3af3b9bb1a516f05a9075abb034a2875b
Curran Bhatia <currankbhatia@gmail.com>
2022-03-16 01:48:09 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+function DisplayResult() {
+  // const photos = [];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myDiv">
+      <h1>Result of Contract Execution</h1>
+    </div>
+  );
+}
+
+function Intake(props) {
+
+
+  const handleTextChange = (event) => {
+    const value = event.target.value;
+    //setTextTitle(value);
+    console.log("value: " + value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // console.log("handleSubmit");
+    // console.log("text: " + textTitle);
+    // console.log("imgTitle: " + imgTitle);
+
+    props.onIntake("succeeded");
+  };
+
+  return (
+    <div className="myDiv">
+      <h1>Execute Smart Contract</h1>
+      <form className = "form-c" action="/action_page.php" onSubmit={handleSubmit}>
+        <p> Name: <input type="text" onChange={handleTextChange}/> First Number: <input type="text" onChange={handleTextChange}/> Second Number: <input type="text" onChange={handleTextChange}/> </p>
+        <input type="submit" value="Submit"/>        
+      </form>
+    </div>
+  );
+}
+
+
+function App(props) {
+
+  const [showIntake, setShowIntake] = React.useState(true); //Setting this to False for now
+
+  const handleIntake = (text) => {
+    console.log("text: " + text);
+    if (text === "succeeded") {
+      setShowIntake(false)
+    }
+  };
+
+  return (
+    <div>
+    {showIntake &&
+      <Intake onIntake={handleIntake}/>
+    }
+    {!showIntake &&
+      <DisplayResult />
+    }
     </div>
   );
 }
